c8c192bce0c57514fa18508e8872c486adeab41b
Saurabh Yadav <saurabhyadav7041916@gmail.com>
2026-02-25 16:13:03 +0530

    ss

--- src/api/resInterceptor.ts
+++ src/api/resInterceptor.ts
@@ -10,10 +10,15 @@ api.interceptors.response.use(
 
         return response;
     },
-    (error)=>{
+    async (error)=>{
     if(error.response?.status===401){
         // session expired 
 
+        await api.post("/refreshToken").then((data)=>{
+
+        }).catch((err)=>{
+alert("session over, log in again");
+        })
         // reloggin
     }
     }
