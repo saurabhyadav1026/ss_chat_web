7e6c12d50b7a931776020c0cb6eb3bda0e0adc38
Saurabh Yadav <saurabhyadav7041916@gmail.com>
2026-02-25 16:23:59 +0530

    kk

--- src/api/resInterceptor.ts
+++ src/api/resInterceptor.ts
@@ -14,10 +14,10 @@ api.interceptors.response.use(
     if(error.response?.status===401){
         // session expired 
 
-        await api.post("/refreshToken").then((data)=>{
-
-        }).catch((err)=>{
-alert("session over, log in again");
+        await api.post("/refreshToken").then((data:any)=>{
+console.log(data);
+        }).catch((err:any)=>{
+alert("session over, log in again"+err);
         })
         // reloggin
     }
