e833a878e46b458b89acd9afffbaef8b0d8f8eb2
Saurabh Yadav <saurabhyadav7041916@gmail.com>
2026-06-01 18:27:43 +0530

    socket config cors fix

--- src/contexts/socketcontext/SocketContext.tsx
+++ src/contexts/socketcontext/SocketContext.tsx
@@ -5,9 +5,9 @@ import { createContext } from 'react';
 import io from 'socket.io-client';
 
 const SocketContext = createContext({});
-export const socket = io(import.meta.env.VITE_API_KEY+"/u", {
-  // autoConnect:false
-
+export const socket = io(import.meta.env.VITE_API_KEY+"/u",{
+  transports: ["websocket", "polling"],
+  withCredentials: true,
 });
 export const funChatSocket=io(import.meta.env.VITE_API_KEY+"/funchat");
 export const callSocket=io(import.meta.env.VITE_API_KEY+"/call");
