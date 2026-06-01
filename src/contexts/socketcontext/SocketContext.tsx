5f0651d2a216b693d992c9668d46406da8eb838a
Saurabh Yadav <saurabhyadav7041916@gmail.com>
2026-06-01 19:21:02 +0530

    socket 2rrr

--- src/contexts/socketcontext/SocketContext.tsx
+++ src/contexts/socketcontext/SocketContext.tsx
@@ -2,15 +2,21 @@
 
 import { createContext } from 'react';
 
-import io from 'socket.io-client';
+import {io} from 'socket.io-client';
 
 const SocketContext = createContext({});
 export const socket = io(import.meta.env.VITE_API_KEY+"/u",{
   transports: ["websocket", "polling"],
   withCredentials: true,
 });
-export const funChatSocket=io(import.meta.env.VITE_API_KEY+"/funchat");
-export const callSocket=io(import.meta.env.VITE_API_KEY+"/call");
+export const funChatSocket=io(import.meta.env.VITE_API_KEY+"/funchat",{
+  transports: ["websocket", "polling"],
+  withCredentials: true,
+});
+export const callSocket=io(import.meta.env.VITE_API_KEY+"/call",{
+  transports: ["websocket", "polling"],
+  withCredentials: true,
+});
 
 
 export const SocketContextProvider = ({ children }: any) => {
